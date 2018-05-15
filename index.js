f65c3db50491a502c1405b206d0da638e0a88c23
Alan Marin De Luna <alan.marin1996@email.bakersfieldcollege.edu>
2018-05-15 16:25:54 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create a component
+const App = () => (
+        <Header />
+    );
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
